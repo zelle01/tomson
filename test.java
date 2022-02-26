54ee635879807bfa5584a71bcd96368240438692
Tinan Dia Lou <aminata3011@gmail.com>
2022-02-26 20:53:30 +0000

    testing files

---
+++ test.java
@@ -0,0 +1,11 @@
+working-area   staging-area   Local-Repo    Remote-Repo  :
+        developement    review
+                        git add       git-commit     git push
+
+         app.java      app.java
+         app.java
+         developement   review         commit         GitHub
+         writing code  
+         local-Computer    LC            LC           
+         file.RED        File.GREEN 
+        git add          git commit     
