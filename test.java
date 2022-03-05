d8aa0ffb9be004f46ba18d21c038ea42d499c50e
bosslady011 <99775984+bosslady011@users.noreply.github.com>
2022-03-05 10:33:07 -0600

    Update test.java

--- test.java
+++ test.java
@@ -1,11 +1,3 @@
-working-area   staging-area   Local-Repo    Remote-Repo  :
-        developement    review
-                        git add       git-commit     git push
-
-         app.java      app.java
-         app.java
-         developement   review         commit         GitHub
-         writing code  
-         local-Computer    LC            LC           
-         file.RED        File.GREEN 
-        git add          git commit     
+Hello World,do you know that every success is a combinataion of two things: your efforts and GOD's help.
+Also keep in mind that wherever is meant to be will be, so you meant to be in this class and become the best dev0ps Engineer in the field.
+Be patient and put the efforts, the result will surely come GOD willing
